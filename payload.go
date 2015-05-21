03022a31737358c9f4926f4fdecdff06b8b0b2ee
Zubair Hamed <zubairhamed@gmail.com>
2015-05-21 09:29:56 +0800

    Empty Payload

--- payload.go
+++ payload.go
@@ -74,7 +74,24 @@ type XmlPayload struct {
 
 }
 
+func NewEmptyPayload() (MessagePayload) {
+    return &EmptyPayload{}
+}
+
 type EmptyPayload struct {
 
 }
+
+func (p *EmptyPayload) GetBytes() ([]byte) {
+    return []byte{}
+}
+
+func (p *EmptyPayload) Length() (int) {
+    return 0
+}
+
+func (p *EmptyPayload) String() (string) {
+    return ""
+}
+
 
