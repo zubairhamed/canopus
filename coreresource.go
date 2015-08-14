daae49994799704fcfede8045ea50fcc64e0591a
Zubair Hamed <zubairhamed@gmail.com>
2015-08-14 18:49:43 +0800

    documentation for code

--- coreresource.go
+++ coreresource.go
@@ -1,5 +1,6 @@
 package canopus
 
+// Instantiates a new core-attribute with a given key/value
 func NewCoreAttribute(key string, value interface{}) *CoreAttribute {
 	return &CoreAttribute{
 		Key:   key,
@@ -12,6 +13,7 @@ type CoreAttribute struct {
 	Value interface{}
 }
 
+// Instantiates a new Core Resource Object
 func NewCoreResource() *CoreResource {
 	c := &CoreResource{}
 
--- event.go
+++ event.go
@@ -35,6 +35,8 @@ func NewCanopusEvents() *CanopusEvents {
 	}
 }
 
+// This holds the various events which are triggred throughout
+// an application's lifetime
 type CanopusEvents struct {
 	evtFnNotify        []FnEventNotify
 	evtFnStart         []FnEventStart
@@ -46,80 +48,97 @@ type CanopusEvents struct {
 	evtFnMessage       []FnEventMessage
 }
 
+// Fired when an observeed resource is notified
 func (ce *CanopusEvents) OnNotify(fn FnEventNotify) {
 	ce.evtFnNotify = append(ce.evtFnNotify, fn)
 }
 
+// Fired when the server/client starts up
 func (ce *CanopusEvents) OnStart(fn FnEventStart) {
 	ce.evtFnStart = append(ce.evtFnStart, fn)
 }
 
+// Fired when the server/client closes
 func (ce *CanopusEvents) OnClose(fn FnEventClose) {
 	ce.evtFnClose = append(ce.evtFnClose, fn)
 }
 
+// Fired when a discovery request is triggered
 func (ce *CanopusEvents) OnDiscover(fn FnEventDiscover) {
 	ce.evtFnDiscover = append(ce.evtFnDiscover, fn)
 }
 
+// Catch-all event which is fired when an error occurs
 func (ce *CanopusEvents) OnError(fn FnEventError) {
 	ce.evtFnError = append(ce.evtFnError, fn)
 }
 
+// Fired when an observe request is triggered for a resource
 func (ce *CanopusEvents) OnObserve(fn FnEventObserve) {
 	ce.evtFnObserve = append(ce.evtFnObserve, fn)
 }
 
+// Fired when an observe-cancel request is triggered foa r esource
 func (ce *CanopusEvents) OnObserveCancel(fn FnEventObserveCancel) {
 	ce.evtFnObserveCancel = append(ce.evtFnObserveCancel, fn)
 }
 
+// Fired when a message is received
 func (ce *CanopusEvents) OnMessage(fn FnEventMessage) {
 	ce.evtFnMessage = append(ce.evtFnMessage, fn)
 }
 
+// Fires the "OnNotify" event
 func (ce *CanopusEvents) Notify(resource string, value interface{}, msg *Message) {
 	for _, fn := range ce.evtFnNotify {
 		fn(resource, value, msg)
 	}
 }
 
+// Fires the "OnStarted" event
 func (ce *CanopusEvents) Started(server *CoapServer) {
 	for _, fn := range ce.evtFnStart {
 		fn(server)
 	}
 }
 
+// Fires the "OnClosed" event
 func (ce *CanopusEvents) Closed(server *CoapServer) {
 	for _, fn := range ce.evtFnClose {
 		fn(server)
 	}
 }
 
+// Fires the "OnDiscover" event
 func (ce *CanopusEvents) Discover() {
 	for _, fn := range ce.evtFnDiscover {
 		fn()
 	}
 }
 
+// Fires the "OnError" event
 func (ce *CanopusEvents) Error(err error) {
 	for _, fn := range ce.evtFnError {
 		fn(err)
 	}
 }
 
+// Fires the "OnObserve" event
 func (ce *CanopusEvents) Observe(resource string, msg *Message) {
 	for _, fn := range ce.evtFnObserve {
 		fn(resource, msg)
 	}
 }
 
+// Fires the "OnObserveCancelled" event for a given resource
 func (ce *CanopusEvents) ObserveCancelled(resource string, msg *Message) {
 	for _, fn := range ce.evtFnObserveCancel {
 		fn(resource, msg)
 	}
 }
 
+// Fires the "OnMessage" event. The 'inbound' variables determines if the
+// message is inbound or outgoing
 func (ce *CanopusEvents) Message(msg *Message, inbound bool) {
 	for _, fn := range ce.evtFnMessage {
 		fn(msg, inbound)
