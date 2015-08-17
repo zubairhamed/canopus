9e055879270d287e5b7d1ed32c1de3417cba68ce
Zubair Hamed <zubairhamed@gmail.com>
2015-08-17 21:44:56 +0800

    Fix tests

--- response_test.go
+++ response_test.go
@@ -12,7 +12,7 @@ func TestResponse(t *testing.T) {
 
 	response := NewResponse(msg, ERR_UNKNOWN_CRITICAL_OPTION)
 	assert.NotNil(t, response)
-	assert.Equal(t, uint16(12345), response.msg.MessageId)
-	assert.Equal(t, ERR_UNKNOWN_CRITICAL_OPTION, response.err)
+	assert.Equal(t, uint16(12345), response.GetMessage().MessageId)
+	assert.Equal(t, ERR_UNKNOWN_CRITICAL_OPTION, response.GetError())
 
 }
