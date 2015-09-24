b8adfc85e024b7418a96b6b3a66f7f0a79a67010
Zubair Hamed <zubairhamed@gmail.com>
2015-09-24 09:59:26 +0800

    Add test for Issue #63 Added Message.GetTokenString()

--- server_test.go
+++ server_test.go
@@ -16,3 +16,27 @@ func TestServerInstantiate(t *testing.T) {
 	assert.Equal(t, 5683, s.localAddr.Port)
 	assert.Equal(t, "udp", s.localAddr.Network())
 }
+
+func TestDiscoveryService(t *testing.T) {
+	server := NewLocalServer()
+	assert.NotNil(t, server)
+	assert.Equal(t, 5683, server.localAddr.Port)
+	assert.Equal(t, "udp", server.localAddr.Network())
+
+	go server.Start()
+	client := NewCoapServer(":0")
+	client.OnStart(func(server *CoapServer) {
+		tok := "abc123"
+		client.Dial("localhost:5683")
+
+		req := NewRequest(TYPE_CONFIRMABLE, GET, GenerateMessageId())
+		req.SetToken(tok)
+		req.SetRequestURI(".well-known/core")
+		resp, err := client.Send(req)
+		assert.Nil(t, err)
+
+		assert.Equal(t, tok, resp.GetMessage().GetTokenString())
+		client.Stop()
+	})
+	client.Start()
+}
